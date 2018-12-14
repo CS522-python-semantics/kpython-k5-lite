a0041de9a504414234029424b90003556f3e214b
Qianyang Peng <qp3@illinois.edu>
2018-12-14 00:01:34 -0800

    mote tests

--- python/tests/basicClassTest.py
+++ python/tests/basicClassTest.py
@@ -1,6 +1,13 @@
 class TestBasic:
+    def __init(self)__:
+        self.a = 8
+        self.b = 10
     def Sum(self, a, b):
-        return a + b
+        return a + b + self.a + self.b
+    def Sum2(self, c, d):
+        a = c
+        b = d
+        return a + b + self.a + self.b
 
 a = TestBasic()
 b = a.Sum(1, 3)
---
+++ python/tests/constructorTest.py
@@ -0,0 +1,38 @@
+# Testing new, constructor, method invocation.
+
+class C:
+  i = 0
+  j = 0
+  def __init__(self, x):
+    self.i = x
+    self.j = x+1
+  def add(self, d):
+    self.i = self.i+d
+    self.j = self.j-d
+  def print2(self):
+    print(self.i, " ", self.j, "\n")
+
+if __name__ == '__main__':
+    a = b = 5
+    c = [[2,3],2]*2
+    h = [2,3,4,5]
+    h[3] = 88
+    k = range(5)
+    w = range(2,5)
+    f = range(1,6,3)
+    r = 100 if (a-3 not in f) else 500
+    i = 0
+    while i < 5:
+      i += 1
+    o = C(a)
+    if isinstance(o,C):
+      print ("yes")
+    o.print2()
+    o.add(b+1)
+    o.print2()
+    d = c[0]
+    e = a
+    print (c)
+
+# 5 6
+# 11 0
